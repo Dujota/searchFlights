ff1cd427e8fac055ba99ad943b9199c8b4c24ea4
Denis Sanchez Dujota <e_scriba@hotmail.com>
2018-03-15 04:21:47 -0400

    variables to be used added

--- app.rb
+++ app.rb
@@ -3,6 +3,14 @@ require "csv"
 
 get '/searchFlights/:origin/:destination' do
 
+  # save inputs from http request
+  orig = params[:origin]
+  dest = params[:destination]
+
+  # Containers for data
+  matches = []
+  sorted_matches = []
+  final_output = []
 
 
 
